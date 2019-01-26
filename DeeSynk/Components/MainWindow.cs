3ed8ed8c5d914297668e91a7a66b8d20cecba0c1
Samuel Golden <samgolden00@gmail.com>
2019-01-26 02:59:30 -0600

    MainWindow important functions and descriptions

--- DeeSynk/Components/MainWindow.cs
+++ DeeSynk/Components/MainWindow.cs
@@ -4,24 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
 
 namespace DeeSynk
 {
     public sealed class MainWindow : GameWindow
     {
 
-        public MainWindow() : base( 500,                                    // initial width
+        private OpenTK.Input.KeyboardState keyState;    // holds current keyboard state, updated every frame
+        private Color4 clearColor = Color4.White;     // the color that OpenGL uses to clear the color buffer on each frame
+        
+
+        /// <summary>
+        /// Basic constructor for the game window. The base keyword allows parameters to be
+        /// passed to the parent class constructor. The title of the window is then set with
+        /// additional information, including the current OpenGL version.
+        /// </summary>
+        public MainWindow() : base( 700,                                    // initial width
                                     500,                                    // initial height
-                                    OpenTK.Graphics.GraphicsMode.Default,
+                                    GraphicsMode.Default,
                                     "DeeSynk",                              // initial window title
                                     GameWindowFlags.Default,
                                     DisplayDevice.Default,
                                     4,                                      // OpenGL major version
                                     0,                                      // OpenGL minor version
-                                    OpenTK.Graphics.GraphicsContextFlags.ForwardCompatible)
+                                    GraphicsContextFlags.ForwardCompatible)
         {
             Title += " | The WIP Student Video Game | OpenGL Version: " + GL.GetString(StringName.Version);
+        }
+        
+        /// <summary>
+        /// OnResize is called if the user decides to resize the window. It will reset the viewport
+        /// so that OpenGL knows the new size of the window.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        /// <summary>
+        /// The OnLoad method gets called as soon as the window is created. Anything that needs to be
+        /// created or initialized at the start of the game should go here.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            CursorVisible = true;
+        }
+
+        /// <summary>
+        /// The OnUpdateFrame method gets called on every frame. Anything that needs to be updated or
+        /// changed on every frame should go here. Drawing does NOT go here.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            HandleKeyboard();
+        }
+
+        /// <summary>
+        /// The OnRenderFrame method gets called on every frame. Anything drawn and rendered on each frame
+        /// should go here. Data changes and updates do NOT go here.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            Title = $"DeeSynk | OpenGL Version: {GL.GetString(StringName.Version)} | Vsync: {VSync} | FPS: {1f / e.Time:0}";
+
+            GL.ClearColor(clearColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
+
+        /// <summary>
+        /// The HandleKeyboard method listens for any keyboard inputs from the user. Anything dealing
+        /// with keybindings should go here.
+        /// </summary>
+        private void HandleKeyboard()
+        {
+            keyState = OpenTK.Input.Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Key.Escape))
+                Exit();
         }
     }
 }
