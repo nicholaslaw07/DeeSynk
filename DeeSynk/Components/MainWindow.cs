2d79d9573da1e232b36f8142219c56a2c50c334c
Samuel Golden <samgolden00@gmail.com>
2019-01-25 23:26:59 -0600

    Created MainWindow class and basic constructor. Now runnable.

---
+++ DeeSynk/Components/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+namespace DeeSynk
+{
+    public sealed class MainWindow : GameWindow
+    {
+
+        public MainWindow() : base( 500,                                    // initial width
+                                    500,                                    // initial height
+                                    OpenTK.Graphics.GraphicsMode.Default,
+                                    "DeeSynk",                              // initial window title
+                                    GameWindowFlags.Default,
+                                    DisplayDevice.Default,
+                                    4,                                      // OpenGL major version
+                                    0,                                      // OpenGL minor version
+                                    OpenTK.Graphics.GraphicsContextFlags.ForwardCompatible)
+        {
+            Title += " | The WIP Student Video Game | OpenGL Version: " + GL.GetString(StringName.Version);
+        }
+    }
+}
--- DeeSynk/Program.cs
+++ DeeSynk/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// The Program class acts as a driver for the entirety of the game.
+/// It instantiates an instance of the MainWindow class (which inherits from the GameWindow class),
+/// and then immediately calls the Run method with a specified FPS, which starts the game. 
+/// </summary>
+
 namespace DeeSynk
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            new MainWindow().Run(60);
         }
     }
 }
