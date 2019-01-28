150a1435c9d829b873ebeb66b121f033b58a5c44
Samuel Golden <samgolden00@gmail.com>
2019-01-28 00:42:12 -0600

    Added Boss and Game class skeletons

---
+++ DeeSynk/Components/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeeSynk.Components
+{
+    /// <summary>
+    /// All objects and mechanic modeling should be housed here.
+    /// </summary>
+    public class Game
+    {
+        public Game()
+        {
+
+        }
+    }
+}
