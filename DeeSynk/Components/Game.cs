0068eec3969dc228364363737ffbf8a365a3a773
Samuel Golden <samgolden00@gmail.com>
2019-01-29 22:50:48 -0600

    Removed test line

--- DeeSynk/Components/Game.cs
+++ DeeSynk/Components/Game.cs
@@ -11,7 +11,6 @@ namespace DeeSynk.Components
     /// </summary>
     public class Game
     {
-        private readonly Managers.ShaderManager shaderManager;
         public Game()
         {
             
