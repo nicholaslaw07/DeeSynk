446ed7177a9796cca2ebfdb4924c94febccc81f8
nicholaslaw07 <nicholastahoe@yahoo.com>
2019-02-21 22:16:19 -0500

    Fixed a semi-colon error.

--- DeeSynk/Components/MainWindow.cs
+++ DeeSynk/Components/MainWindow.cs
@@ -59,7 +59,6 @@ namespace DeeSynk.Components
             _game = new Game();
             _game.LoadAll();
             CursorVisible = true;
-            Matrix4.CreateScale()
         }
 
         /// <summary>
