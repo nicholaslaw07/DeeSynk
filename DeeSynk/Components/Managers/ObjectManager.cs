4ee1909f15a5067555cf1670000c785136722b9f
Samuel Golden <samgolden00@gmail.com>
2019-02-22 20:01:05 -0600

    ObjectManager skeleton

---
+++ DeeSynk/Components/Managers/ObjectManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeeSynk.Components.Managers
+{
+    class ObjectManager : IManager
+    {
+        private static ObjectManager _objectManager;
+
+        private ObjectManager()
+        {
+
+        }
+
+        public static ref ObjectManager GetInstance()
+        {
+            if (_objectManager == null)
+                _objectManager = new ObjectManager();
+
+            return ref _objectManager;
+        }
+
+        public void Load()
+        {
+
+        }
+         public void UnLoad()
+        {
+            
+        }
+    }
+}
