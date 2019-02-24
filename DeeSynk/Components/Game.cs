255b9f700b79261cb32cb61749557c8cca830c3b
Samuel Golden <samgolden00@gmail.com>
2019-02-24 16:37:21 -0600

    Some stress testing

--- DeeSynk/Components/Game.cs
+++ DeeSynk/Components/Game.cs
@@ -26,12 +26,35 @@ namespace DeeSynk.Components
             int program = Managers.ShaderManager.GetInstance().GetProgram("defaultColored");
             int[] programs = { program };
 
+            Random rand = new Random();
+
             Managers.ObjectManager om = Managers.ObjectManager.GetInstance();
-            om.CreateCircle(1, 0, 0, 160, Color4.Black).AddProgramIDs(programs).InitializeVAO();
-            om.CreateRectangle(1, 100, 100, -55, 55, Color4.Red).AddProgramIDs(programs).InitializeVAO();
-            om.CreateRectangle(1, 100, 100, -55, -55, Color4.Yellow).AddProgramIDs(programs).InitializeVAO();
-            om.CreateRectangle(1, 100, 100, 55, 55, Color4.Green).AddProgramIDs(programs).InitializeVAO();
-            om.CreateRectangle(1, 100, 100, 55, -55, Color4.Blue).AddProgramIDs(programs).InitializeVAO();
+            for(int i=0; i < 10000; i++)
+            {
+                om.CreateCircle(1, rand.Next(-500, 500), rand.Next(-500, 500), 10, Color4.Blue).AddProgramIDs(programs).InitializeVAO();
+            }
+            Console.WriteLine("CREATION 1");
+            for(int i=0; i < 10000; i++)
+            {
+                om.DeleteGameObject(i);
+            }
+            Console.WriteLine("DELETION 1");
+            for(int i=0; i < 10000; i++)
+            {
+                om.CreateCircle(1, rand.Next(-500, 500), rand.Next(-500, 500), 10, Color4.Red).AddProgramIDs(programs).InitializeVAO();
+            }
+            Console.WriteLine("CREATION 2");
+            for(int i=4000; i<5000; i++)
+            {
+                om.DeleteGameObject(i);
+            }
+            Console.WriteLine("DELETION 2");
+            for(int i=0; i < 1000; i++)
+            {
+                om.CreateCircle(1, rand.Next(-500, 500), rand.Next(-500, 500), 10, Color4.Green).AddProgramIDs(programs).InitializeVAO();
+            }
+            Console.WriteLine("CREATION 3");
+            //om.CreateRectangle(1, 100, 100, 55, -55, Color4.Blue).AddProgramIDs(programs).InitializeVAO();
 
 
             /* END TESTING */
@@ -43,6 +66,7 @@ namespace DeeSynk.Components
             Managers.ShaderManager.GetInstance().Load();
             Managers.ObjectManager.GetInstance().Load();
             Managers.TextureManager.GetInstance().Load();
+            Console.WriteLine(GL.GetString(StringName.Renderer));
         }
 
         public void LoadGameData()
