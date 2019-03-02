eb01ecc1903de7aaf01b9a921499c54b01197471
Samuel Golden <samgolden00@gmail.com>
2019-03-01 18:41:45 -0600

    remove stress testing

--- DeeSynk/Core/Game.cs
+++ DeeSynk/Core/Game.cs
@@ -18,47 +18,6 @@ namespace DeeSynk.Core
         public Game()
         {
             Load();
-
-            /* TESTING */
-
-            // not sure how to handle these programs yet, I suspect a component based approach for
-            // renderability is the answer.
-            int program = Managers.ShaderManager.GetInstance().GetProgram("defaultColored");
-            int[] programs = { program };
-
-            Random rand = new Random();
-
-            Managers.ObjectManager om = Managers.ObjectManager.GetInstance();
-            for(int i=0; i < 10000; i++)
-            {
-                om.CreateCircle(1, rand.Next(-500, 500), rand.Next(-500, 500), 10, Color4.Blue).AddProgramIDs(programs).InitializeVAO();
-            }
-            Console.WriteLine("CREATION 1");
-            for(int i=0; i < 10000; i++)
-            {
-                om.DeleteGameObject(i);
-            }
-            Console.WriteLine("DELETION 1");
-            for(int i=0; i < 10000; i++)
-            {
-                om.CreateCircle(1, rand.Next(-500, 500), rand.Next(-500, 500), 10, Color4.Red).AddProgramIDs(programs).InitializeVAO();
-            }
-            Console.WriteLine("CREATION 2");
-            for(int i=4000; i<5000; i++)
-            {
-                om.DeleteGameObject(i);
-            }
-            Console.WriteLine("DELETION 2");
-            for(int i=0; i < 1000; i++)
-            {
-                om.CreateCircle(1, rand.Next(-500, 500), rand.Next(-500, 500), 10, Color4.Green).AddProgramIDs(programs).InitializeVAO();
-            }
-            Console.WriteLine("CREATION 3");
-            //om.CreateRectangle(1, 100, 100, 55, -55, Color4.Blue).AddProgramIDs(programs).InitializeVAO();
-
-
-            /* END TESTING */
-
         }
 
         public void Load()
