2ba31bcdcc0e0de4c9a4171a9fff2adc869439bc
Nicholas Lawrence <nicholastahoe@yahoo.com>
2019-03-02 21:04:51 -0500

    Added some constructors to components and added a method in GameObject to check if it contains certain components via bitwise and.

--- DeeSynk/Core/Components/Types/ComponentLocation.cs
+++ DeeSynk/Core/Components/Types/ComponentLocation.cs
@@ -8,5 +8,36 @@ namespace DeeSynk.Core.Components.Types
 
         private Vector4 _location;
         public Vector4 Location { get => _location; set => _location = value; }
+
+        public ComponentLocation()
+        {
+            _location = new Vector4();
+        }
+
+        public ComponentLocation(float X, float Y, float Z, float W)
+        {
+            _location = new Vector4(X, Y, Z, W);
+        }
+        public ComponentLocation(float X, float Y, float Z)
+        {
+            _location = new Vector4(X, Y, Z, 1.0f);
+        }
+        public ComponentLocation(float X, float Y)
+        {
+            _location = new Vector4(X, Y, 1.0f, 1.0f);
+        }
+
+        public ComponentLocation(ref Vector4 l)
+        {
+            _location = new Vector4(l.X, l.Y, l.Z, l.W);
+        }
+        public ComponentLocation(ref Vector3 l)
+        {
+            _location = new Vector4(l.X, l.Y, l.Z, 1.0f);
+        }
+        public ComponentLocation(ref Vector2 l)
+        {
+            _location = new Vector4(l.X, l.Y, 1.0f, 1.0f);
+        }
     }
 }
