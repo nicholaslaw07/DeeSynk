9d476c6c10766ca2f9156f36238a240d718a5c14
Nicholas Lawrence <nicholastahoe@yahoo.com>
2019-03-05 05:02:39 -0500

    Rendering stuff. The world is great.

--- DeeSynk/Core/Components/Types/Render/ComponentRender.cs
+++ DeeSynk/Core/Components/Types/Render/ComponentRender.cs
@@ -16,6 +16,12 @@ namespace DeeSynk.Core.Components.Types.Render
         /// </summary>
         public int VAO_ID { get => _vaoID; }
 
+        private int _iboID;
+        /// <summary>
+        /// The ID of the IBO that this object should be rendered with.
+        /// </summary>
+        public int IBO_ID { get => _iboID; }
+
         private int _shaderID;
         /// <summary>
         /// The ID of the shader that this object should be rendered with.
@@ -27,6 +33,13 @@ namespace DeeSynk.Core.Components.Types.Render
         //Is simple sprite?  Idk
         //Has unique VAO
         //Position in vao if not unique
+
+        public ComponentRender(int vaoID, int iboID, int shaderID)
+        {
+            _vaoID = vaoID;
+            _iboID = iboID;
+            _shaderID = shaderID;
+        }
 
         public void Update(float time)
         {
--- DeeSynk/Core/Systems/SystemRender.cs
+++ DeeSynk/Core/Systems/SystemRender.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DeeSynk.Core.Components;
 using DeeSynk.Core.Components.Types.Render;
+using OpenTK.Graphics.OpenGL4;
 
 namespace DeeSynk.Core.Systems
 {
@@ -23,6 +24,8 @@ namespace DeeSynk.Core.Systems
         private ComponentModel[]        _modelComps;
         private ComponentTexture[]      _textureComps;
         private ComponentColor[]        _colorComps;
+
+        int ISystem.MonitoredComponents => throw new NotImplementedException();
 
         public SystemRender(World world)
         {
@@ -50,11 +53,60 @@ namespace DeeSynk.Core.Systems
                     }
                 }
             }
+        }
 
+        public void InitModels()
+        {
+            for(int i=0; i<_renderComps.Length; i++)
+            {
+                _modelComps[i] = new ComponentModel(100f, 100f);
+            }
+        }
+
+        public void InitVAO()
+        {
+            int shaderID = 0;
+            for(int i=0; i<_renderComps.Length; i++)
+            {
+                int vao = GL.GenVertexArray();
+                GL.BindVertexArray(vao);
+
+                int vbo = GL.GenBuffer();
+                int cbo = GL.GenBuffer();
+                int ibo = GL.GenBuffer();
+
+                GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, cbo);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, ibo);
+
+                GL.NamedBufferStorage(vbo, 4 * _modelComps[i].VertexCount, _modelComps[i].GetVertices, BufferStorageFlags.MapWriteBit);
+
+                GL.VertexArrayAttribBinding(vao, 0, 0);
+                GL.EnableVertexArrayAttrib(vao, 0);
+                GL.VertexArrayAttribFormat(vao, 0, 4, VertexAttribType.Float, false, 0);
+
+                GL.VertexArrayVertexBuffer(vao, 0, vbo, IntPtr.Zero, 4);
+                GL.NamedBufferStorage(ibo, 4 * _modelComps[i].IndexCount, _modelComps[i].GetIndices, BufferStorageFlags.MapWriteBit);
+
+                GL.BindVertexArray(0);
+
+                _renderComps[i] = new ComponentRender(vao, ibo, shaderID);
+            }
         }
 
         public void Update(float time)
         {
+        }
+
+        public void Render()
+        {
+            for(int i=0; i<_renderComps.Length; i++)
+            {
+                GL.BindVertexArray(_renderComps[i].VAO_ID);
+                GL.UseProgram(_renderComps[i].SHADER_ID);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, _renderComps[i].IBO_ID);
+                GL.DrawArrays(PrimitiveType.Quads, 0, 1);
+            }
         }
     }
 }
