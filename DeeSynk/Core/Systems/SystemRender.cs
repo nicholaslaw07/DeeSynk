6b29dd41bc1ba1b4c01b5be2e52296ef11e29782
Samuel Golden <samgolden00@gmail.com>
2019-03-05 03:16:19 -0600

    world updated to support SystemRender

--- DeeSynk/Core/Systems/SystemRender.cs
+++ DeeSynk/Core/Systems/SystemRender.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DeeSynk.Core.Components;
+using DeeSynk.Core.Components.Types.Render;
 
 namespace DeeSynk.Core.Systems
 {
     class SystemRender : ISystem
     {
-        public int MonitoredComponents => throw new NotImplementedException();
+        public int MonitoredComponents = (int)Component.RENDER |
+                                         (int)Component.MODEL |
+                                         (int)Component.TEXTURE |
+                                         (int)Component.COLOR;
+
+        private World _world;
+        
+        private ComponentRender[]       _renderComps;
+        private ComponentModel[]        _modelComps;
+        private ComponentTexture[]      _textureComps;
+        private ComponentColor[]        _colorComps;
+
+        public SystemRender(World world)
+        {
+            _world = world;
+
+        }
 
         public void Update(float time)
         {
