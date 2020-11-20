1ff730bfac6c242890645cd8e4516aae10eeeed0
Nicholas Lawrence <nicholastahoe@yahoo.com>
2020-11-20 13:28:58 -0500

    Minor restructure.  Moved all OpenType font stuff to a OpenType folder inside of fonts.  Will need to change the namespace over to OpenType.  Added a bunch of blank classes for the remaining tables.  Need to add data checking on data inputed for the OS2 table.

---
+++ DeeSynkPort/Core/Components/Fonts/OpenType/Tables/Cmap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeeSynk.Core.Components.Fonts.Tables
+{
+    public struct EncodingRecord
+    {
+
+    }
+    public class Cmap
+    {
+        //header
+
+        //data
+
+    }
+}
---
+++ DeeSynkPort/Core/Components/Fonts/OpenType/Tables/Head.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeeSynk.Core.Components.Fonts.OpenType.Tables
+{
+    public class Head
+    {
+    }
+}
